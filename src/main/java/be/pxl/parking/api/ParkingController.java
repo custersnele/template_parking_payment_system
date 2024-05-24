573814049238ed572301e94869e837a8cc766a86
cust3rsn3l3 <custersnele@gmail.com>
2024-05-24 15:11:50 +0200

    update response

--- src/main/java/be/pxl/parking/api/ParkingController.java
+++ src/main/java/be/pxl/parking/api/ParkingController.java
@@ -3,6 +3,7 @@ package be.pxl.parking.api;
 import be.pxl.parking.api.input.ParkingSessionStartCommand;
 import be.pxl.parking.api.input.ParkingSessionStopCommand;
 import be.pxl.parking.service.ParkingService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,12 +19,14 @@ public class ParkingController {
 
 
     @PostMapping("/parking/start")
-    public void startParkingSession(@RequestBody ParkingSessionStartCommand command) {
+    public ResponseEntity<Void> startParkingSession(@RequestBody ParkingSessionStartCommand command) {
         parkingService.startParkingSession(command);
+        return ResponseEntity.accepted().build();
     }
 
     @PostMapping("/parking/stop")
-    public void stopParkingSession(@RequestBody ParkingSessionStopCommand command) {
+    public ResponseEntity<Void> stopParkingSession(@RequestBody ParkingSessionStopCommand command) {
         parkingService.stopParkingSession(command);
+        return ResponseEntity.accepted().build();
     }
 }
